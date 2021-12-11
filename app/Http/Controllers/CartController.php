35c2b9654740fb25d42af55c05ac7a6049396fbb
Reymar Bongolto <ramyer88@gmail.com>
2021-12-11 15:58:05 +0800

    Update cart controller

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,29 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Menu;
 use App\Models\MenuOrder;
 use App\Models\Order;
 use Illuminate\Http\Request;
 
+use function PHPUnit\Framework\returnSelf;
 
 class CartController extends Controller
 {
 
     public function myCart()
     {
-        $orders = Order::with('menu')->where('isServed', 0)->get();
 
+        $orders = Order::with('menu')->where('isServed', 0)->get();
 
         foreach($orders as $order)
         {
-            foreach($order as $menus)
-            {
-                $a = $menus;
-            }
+            $order = $order;
         }
+        //$price = $order->menu->pluck('price');
+         $a = $order;
 
-        dd($a);
-        return view('cart')->with('orders', $orders);
+        $sum = array_reduce($a->menu, function($i, $obj)
+        {
+            return $i += $obj->price;
+        });
+        echo $sum;
+
+        //$a = preg_replace('('.'|\*|\"|\]|\[)','',$price);
+
+
+
+        //return view('cart')->with('orders', $orders);
     }
 
 }
